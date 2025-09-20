c48d36f56ac296e0062e24fbed605abaf0585734
Omar Marwan <omarmarwan123@outlook.com>
2025-09-20 13:33:51 +0400

    Add files via upload

---
+++ Main.c
@@ -0,0 +1,16 @@
+# include "MathFunctions.h"
+
+int add(int firstnum, int secondnum){
+
+    return firstnum + secondnum;
+}
+
+int subtract(int firstnum, int secondnum){
+
+    return firstnum - secondnum;
+}
+
+int multiply(int firstnum, int secondnum){
+
+    return firstnum * secondnum;
+}
